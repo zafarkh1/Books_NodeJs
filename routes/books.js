6102f76a42ea52c5e5166d1cd256b94991ee0a8e
Zafar <hatamovzafar7070@gmail.com>
2024-07-29 17:04:10 +0500

    ended

--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const Joi = require("joi");
+const mongoose = require("mongoose");
 const router = express.Router();
 
-let books = [
-  {id: 1, title: 'book 1', description: 'book 1', price: 200},
-  {id: 2, title: 'book 2', description: 'book 2', price: 300},
-  {id: 3, title: 'book 3', description: 'book 3', price: 400},
-  {id: 4, title: 'book 4', description: 'book 4', price: 500},
-]
+mongoose.connect('mongodb://localhost:27017/project')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err));
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 20,
+    uppercase: true
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 30,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 50,
+    max: 1000,
+    get: v => Math.round(v),
+    set: v => Math.round(v),
+  }
+})
+
+const Book = mongoose.model('Book', bookSchema);
 
 // GET
 router.get('/', (_, res) => {
@@ -22,16 +46,27 @@ router.get('/:id', (req, res) => {
 
 // POST
 router.post('/', (req, res) => {
-  const { error } = validateBook(req.body)
-  if(error) return res.status(400).send(error.details[0].message)
+  const {error} = validateBook(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
-  const book = {
-    id: books.length + 1,
+
+  const book = new Book({
     title: req.body.title,
     description: req.body.description,
-    price: req.body.price,
+    price: req.body.price
+  });
+
+  try {
+    const result =  book.save()
+    console.log(result)
+  } catch (e) {
+    for (let field in e.errors) {
+      console.log(e.errors[field].message)
+    }
   }
-  books.push(book)
+
+
+  postCourse()
   res.send(book)
 })
 
@@ -40,8 +75,8 @@ router.put('/:id', (req, res) => {
   const book = books.find(b => b.id === parseInt(req.params.id))
   if (!book) return res.status(404).send('Book not found!')
 
-  const { error } = validateBook(req.body)
-  if(error) return res.status(400).send(error.details[0].message)
+  const {error} = validateBook(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
   const {title, description, price} = req.body
   book.title = title
@@ -61,7 +96,7 @@ router.delete('/:id', (req, res) => {
   res.send(book)
 })
 
-function validateBook (books) {
+function validateBook(books) {
   const schema = Joi.object().keys({
     title: Joi.string().min(4).required(),
     description: Joi.string().min(4).required(),
